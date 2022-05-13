71967a5f6f61a3a30281c4b80eae51062ea47069
Eric Flodin <91188701+flemt@users.noreply.github.com>
2022-05-13 08:42:58 +0200

    formatted css inqueries

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,5 +1,5 @@
 
- import React, { useState } from "react";
+import React, { useState } from "react";
 import { NavLink } from "react-router-dom";
 import "./NavBar.css";
 
@@ -8,70 +8,62 @@ function NavBar() {
 
   const handleClick = () => setClick(!click);
   return (
-    <>
-    
+    <>    <div className="nav-container">
+      <iframe width="100%" height="80" src="https://rss.app/embed/v1/ticker/09y6cjEy8Mtr1rJv" frameborder="0"></iframe></div>
+      <nav className="navbar">
 
+        <NavLink exact to="/" className="nav-logo">
+          ANDELSBAREN SPORT
+        </NavLink>
+        <ul className={click ? "nav-menu active" : "nav-menu"}>
+          <li className="nav-item">
+            <NavLink
+              exact
+              to="/services"
+              activeClassName="active"
+              className="nav-links"
+              onClick={handleClick}
+            >
+              Tjänster
+            </NavLink>
+          </li>
+          <li className="nav-item">
+            <NavLink
+              exact
+              to="/aboutus"
+              activeClassName="active"
+              className="nav-links"
+              onClick={handleClick}
+            >
+              Om Oss
+            </NavLink>
+          </li>
+          <li className="nav-item">
+            <NavLink
+              exact
+              to="/Media"
+              activeClassName="active"
+              className="nav-links"
+              onClick={handleClick}
+            >
+              Media
+            </NavLink>
+          </li>
+          <li className="nav-item">
+            <NavLink
+              exact
+              to="/contact"
+              activeClassName="active"
+              className="nav-links"
+              onClick={handleClick}
+            >
+              Kontakta oss
+            </NavLink>
+          </li>
+        </ul>
+        <div className="nav-icon" onClick={handleClick}>
+          <i className={click ? "fas fa-times" : "fas fa-bars"}></i>
 
-        <div className="nav-container">     
-           <iframe width="100%" height="80" src="https://rss.app/embed/v1/ticker/09y6cjEy8Mtr1rJv" frameborder="0"></iframe></div>
-           <nav className="navbar">
-        
-          <NavLink exact to="/" className="nav-logo">
-            ANDELSBAREN SPORT
-    
-          </NavLink>
-  
-
-
-          <ul className={click ? "nav-menu active" : "nav-menu"}>
-            <li className="nav-item">
-              <NavLink
-                exact
-                to="/services"
-                activeClassName="active"
-                className="nav-links"
-                onClick={handleClick}
-              >
-             Tjänster
-              </NavLink>
-            </li>
-            <li className="nav-item">
-              <NavLink
-                exact
-                to="/aboutus"
-                activeClassName="active"
-                className="nav-links"
-                onClick={handleClick}
-              >
-                Om Oss
-              </NavLink>
-            </li>
-            <li className="nav-item">
-              <NavLink
-                exact
-                to="/Media"
-                activeClassName="active"
-                className="nav-links"
-                onClick={handleClick}
-              >
-                Media
-              </NavLink>
-            </li>
-            <li className="nav-item">
-              <NavLink
-                exact
-                to="/contact"
-                activeClassName="active"
-                className="nav-links"
-                onClick={handleClick}
-              >
-                Kontakta oss
-              </NavLink>
-            </li>
-          </ul>
-          <div className="nav-icon" onClick={handleClick}>
-            <i className={click ? "fas fa-times" : "fas fa-bars"}></i>
-      
         </div>
       </nav>
     </>
