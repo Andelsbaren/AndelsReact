60ef045dbeaf387dae6579bb3e574a965538978c
Peppo <stefan.krakowsky@studernade-skys.se>
2022-05-16 14:16:24 +0200

    added an amazing slideshow omg

--- src/contents/Media.js
+++ src/contents/Media.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Carousel, {CarouselItem} from './Carousel';
 
 
 class Media extends Component {
@@ -6,7 +7,27 @@ class Media extends Component {
         return (
             <div className="mediapage">
              <p className="kop">Köp andelar</p>
+
+             <div className='Media'>
+                 <Carousel>
+                     <CarouselItem>Item 1</CarouselItem>
+                     <CarouselItem>item 2</CarouselItem>
+                     <CarouselItem>item 3</CarouselItem>
+                     <CarouselItem>Item 4</CarouselItem>
+                     <CarouselItem> Item 5</CarouselItem>
+                     <CarouselItem> Item 6</CarouselItem>
+                     <CarouselItem>Item 7</CarouselItem>
+                     <CarouselItem>Item 8</CarouselItem>
+                 </Carousel>
+             </div>
+
+
+
+
+
+
             </div>
+            
             )
         }
     }
