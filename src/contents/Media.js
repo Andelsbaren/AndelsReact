5ed7d69eb45e37a07faf0f3aa20342b7a4e95099
Peppo <stefan.krakowsky@studernade-skys.se>
2022-05-17 11:20:47 +0200

    added some images to the carousel. still got stuff to fix tho

--- src/contents/Media.js
+++ src/contents/Media.js
@@ -1,5 +1,10 @@
 import React, { Component } from 'react';
 import Carousel, {CarouselItem} from './Carousel';
+import img1 from './images/SVENS.png'
+import img2 from './images/ANDELSSVART.jpg'
+import img3 from './images/AS.jpg'
+import img4 from './images/BAR2018.png'
+import img5 from './images/elitl.jpg'
 
 
 class Media extends Component {
@@ -10,11 +15,11 @@ class Media extends Component {
 
              <div className='Media'>
                  <Carousel>
-                     <CarouselItem>Item 1</CarouselItem>
-                     <CarouselItem>item 2</CarouselItem>
-                     <CarouselItem>item 3</CarouselItem>
-                     <CarouselItem>Item 4</CarouselItem>
-                     <CarouselItem> Item 5</CarouselItem>
+                     <CarouselItem><img src={img1}></img></CarouselItem>
+                     <CarouselItem><img src={img2}></img></CarouselItem>
+                     <CarouselItem><img src={img3}></img></CarouselItem>
+                     <CarouselItem><img src={img4}></img></CarouselItem>
+                     <CarouselItem><img src={img5}></img></CarouselItem>
                      <CarouselItem> Item 6</CarouselItem>
                      <CarouselItem>Item 7</CarouselItem>
                      <CarouselItem>Item 8</CarouselItem>
